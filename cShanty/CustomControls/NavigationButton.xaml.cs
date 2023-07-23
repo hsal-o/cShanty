ca65f1a12c03e81111baaf6ad3ab5b60459d3def
Yumba <hmsalinas31@outlook.com>
2023-07-22 23:01:12 -0500

    Prototype Navigation Bar
    Added prototype navigation bar with core buttons and prototype playlist section

---
+++ cShanty/CustomControls/NavigationButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace cShanty.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for NavigationButton.xaml
+    /// </summary>
+    public partial class NavigationButton : RadioButton
+    {
+        public string Text { get; set; }
+
+        public NavigationButton()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ cShanty/CustomControls/NavigationCoreButton.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace cShanty.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for NavigationButton.xaml
+    /// </summary>
+    public partial class NavigationCoreButton : RadioButton
+    {
+        public string Text { get; set; }
+        public string Kind { get; set; }
+
+        public NavigationCoreButton()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}

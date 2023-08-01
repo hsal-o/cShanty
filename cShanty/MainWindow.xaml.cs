7a1b365aaf7c3866803daf2edaf181d9e7b54a70
yumba <hmsalinas31@outlook.com>
2023-07-31 23:00:01 -0500

    Rough Prototype Media Viewer
    Added functionality to view media files. Prototype only, front end to be designed.

--- cShanty/MainWindow.xaml.cs
+++ cShanty/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace cShanty
 {
@@ -23,6 +26,33 @@ namespace cShanty
         public MainWindow()
         {
             InitializeComponent();
+
+            // Call the method to populate the ListBox with the MP3 files from the "Media" folder
+            PopulateMediaList();
+        }
+
+        private void PopulateMediaList()
+        {
+            string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+            string mediaFolderPath = Path.Combine(currentDirectory, "Media");
+
+            MessageBox.Show("mediaFolderPath: " + mediaFolderPath);
+            Debug.WriteLine("mediaFolderPath: " + mediaFolderPath);
+
+            if (Directory.Exists(mediaFolderPath))
+            {
+                string[] mp3Files = Directory.GetFiles(mediaFolderPath, "*.mp3");
+
+                foreach (string file in mp3Files)
+                {
+                    // Add each MP3 file name to the ListBox
+                    mediaListBox.Items.Add(Path.GetFileName(file));
+                }
+            }
+            else
+            {
+                MessageBox.Show("The 'Media' folder does not exist.");
+            }
         }
     }
 }

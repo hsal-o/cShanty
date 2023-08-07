02bb2962475199f159c2bf97a2eb8a7ce3ca30ee
Yumba <hmsalinas31@outlook.com>
2023-08-06 20:29:21 -0500

    Prototype update
    Simple push to transfer files

---
+++ cShanty/CustomControls/PlayPauseButtonControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cShanty.CustomControls
+{
+    public partial class PlayPauseButtonControl : UserControl, INotifyPropertyChanged
+    {
+        private bool isPlaying = false;
+
+        public bool IsPlaying
+        {
+            get { return isPlaying; }
+            set
+            {
+                isPlaying = value;
+                OnPropertyChanged("PlayPauseIcon");
+            }
+        }
+
+        public string PlayPauseIcon => IsPlaying ? "RegularPause" : "RegularPlay";
+
+        public PlayPauseButtonControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add { playPauseButton.Click += value; }
+            remove { playPauseButton.Click -= value; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- cShanty/MainWindow.xaml.cs
+++ cShanty/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -23,6 +24,25 @@ namespace cShanty
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isMediaPlaying = false; 
+        public ICommand PlayPauseCommand => new RelayCommand(ExecutePlayPause);
+
+        private void ExecutePlayPause()
+        {
+            isMediaPlaying = !isMediaPlaying;
+
+            // Handle play/pause logic here
+            if (isMediaPlaying)
+            {
+                playMedia();
+            }
+            else
+            {
+                pauseMedia();
+            }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,23 +56,73 @@ namespace cShanty
             string currentDirectory = System.IO.Directory.GetCurrentDirectory();
             string mediaFolderPath = Path.Combine(currentDirectory, "Media");
 
-            MessageBox.Show("mediaFolderPath: " + mediaFolderPath);
-            Debug.WriteLine("mediaFolderPath: " + mediaFolderPath);
-
             if (Directory.Exists(mediaFolderPath))
             {
                 string[] mp3Files = Directory.GetFiles(mediaFolderPath, "*.mp3");
 
                 foreach (string file in mp3Files)
                 {
-                    // Add each MP3 file name to the ListBox
-                    mediaListBox.Items.Add(Path.GetFileName(file));
+                    // Add each MP3 file name (without extension) to the ListBox
+                    string fileName = Path.GetFileNameWithoutExtension(file);
+                    ListBoxItem item = new ListBoxItem
+                    {
+                        Content = fileName,
+                        Tag = file // Store the full file path as the Tag property
+                    };
+                    mediaListBox.Items.Add(item);
                 }
             }
             else
             {
                 MessageBox.Show("The 'Media' folder does not exist.");
             }
+        }
+
+        private void restartMedia()
+        {
+            mediaElement.Position = TimeSpan.Zero;
+            mediaElement.Play();
+        }
+
+        private void playMedia()
+        {
+            mediaElement.Play();
+        }
+
+        private void pauseMedia()
+        {
+            mediaElement.Pause();
+        }
+
+        private void restartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            restartMedia();
+        }
+
+        private void pauseBtn_Click(object sender, RoutedEventArgs e)
+        { 
+            pauseMedia();
+        }
+
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            playMedia();
+        }
+
+        private void mediaListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (mediaListBox.SelectedItem != null && mediaListBox.SelectedItem is ListBoxItem selectedItem)
+            {
+                string filePath = selectedItem.Tag as string;
+                mediaElement.Source = new Uri(filePath);
+
+                txt_selectedSong.Text = filePath;
+            }
+        }
+
+        private void PlayPauseButtonControl_Click(object sender, RoutedEventArgs e)
+        {
+            //playPauseButton.IsPlaying = !playPauseButton.IsPlaying;
         }
     }
 }
